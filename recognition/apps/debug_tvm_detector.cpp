adc6b835e6b0b8c77307aa4f7ca91c88d45b09c3
Matt <mkrzus3@gmail.com>
2020-04-23 09:50:40 -0500

    testing initialization from config

--- recognition/apps/debug_tvm_detector.cpp
+++ recognition/apps/debug_tvm_detector.cpp
@@ -937,6 +937,231 @@ class YoloTVMGPU256{
     }
 };
 
+class YoloTVMFromConfig{
+    private:
+        //working: void * handle;
+        std::unique_ptr<tvm::runtime::Module> handle;
+
+    public:
+        // confident in these
+        // set these as the default
+        std::string deploy_lib_path;
+        std::string deploy_graph_path;
+        std::string deploy_param_path;
+        bool gpu = true;
+        int device_id;// = 0;
+        int dtype_code;// = kDLFloat;
+        int dtype_bits;// = 32;
+        int dtype_lanes;// = 1;
+        int device_type;// = kDLGPU;
+        // set default here???
+        int width;// = 512;
+        int height;// = 512;
+        int64_t in_shape[4];// = {1, 3, height, width};
+        int64_t tvm_id_and_score_size[3];// = {1, 100, 1};
+        int64_t tvm_box_size[3];// = {1, 100, 4};
+        int total_input = 3 * width * height;
+        int in_ndim = 4;
+        int out_ndim = 3;
+        int64_t tvm_id_and_score_size[3] = {1, 100, 1};
+        int64_t tvm_box_size[3] = {1, 100, 4};
+
+        YoloTVMFromConfig(std::string config_path, std::string ros_package_string) {
+            // read config with nlohmann-json
+            json model_config;
+            std::string package_path = ros::package::getPath(ros_package_string);
+            std::string full_path = package_path + config_path;
+            std::ifstream json_read(full_path);
+            json_read >> model_config;
+            // read variables
+            deploy_lib_path = config["deploy_lib_path"];
+            deploy_graph_path = config["deploy_graph_path"];
+            deploy_param_path = config["deploy_param_path"];
+            device_id = config["device_id"];
+            width = config["width"];
+            height = config["height"];
+            gpu = config["gpu"];
+            int64_t in_shape[4] = {1, 3, height, width};
+            // set device type
+            if (gpu){
+                device_type = kDLGPU;
+            } else {
+                device_type = kDLCPU;
+            }
+            // read deploy lib
+            tvm::runtime::Module mod_syslib = tvm::runtime::Module::LoadFromFile(deploy_lib_path);
+            // read deplpy json
+            std::ifstream json_in(deploy_graph_path, std::ios::in);
+            std::string json_data((std::istreambuf_iterator<char>(json_in)), std::istreambuf_iterator<char>());
+            json_in.close();
+            // get global function module for graph runtime
+            tvm::runtime::Module mod = (*tvm::runtime::Registry::Get("tvm.graph_runtime.create"))(json_data, mod_syslib,
+                                                                                                  device_type, device_id);
+            this->handle.reset(new tvm::runtime::Module(mod));
+            // parameters in binary
+            std::ifstream params_in(deploy_param_path, std::ios::binary);
+            std::string params_data((std::istreambuf_iterator<char>(params_in)), std::istreambuf_iterator<char>());
+            params_in.close();
+            // parameters need to be TVMByteArray type to indicate the binary data
+            TVMByteArray params_arr;
+            params_arr.data = params_data.c_str();
+            params_arr.size = params_data.length();
+            tvm::runtime::PackedFunc load_params = mod.GetFunction("load_params");
+            load_params(params_arr);
+        }
+       
+        /**
+         * \brief function to normalize an image before it's processed by the network
+         * \param[in] the raw cv::mat image
+         * \return the normalized version of the iamge.
+         */  
+        cv::Mat preprocess_image(cv::Mat frame){
+            cv::Size new_size = cv::Size(width, height); // or is it height width????
+            cv::Mat resized_image;
+            cv::Mat rgb;
+            // bgr to rgb
+            cv::cvtColor(frame, rgb,  cv::COLOR_BGR2RGB);
+            // resize to 512x512
+            cv::resize(rgb, resized_image, new_size);
+            cv::Mat resized_image_floats(new_size, CV_32FC3);
+            // convert resized image to floats and normalize
+            resized_image.convertTo(resized_image_floats, CV_32FC3, 1.0f/255.0f);
+            //mimic mxnets 'to_tensor' function
+            cv::Mat normalized_image(new_size, CV_32FC3);
+            // mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+            cv::Mat mean(new_size, CV_32FC3, cv::Scalar(0.485, 0.456, 0.406));
+            cv::Mat theta(new_size, CV_32FC3, cv::Scalar(0.229, 0.224, 0.225));
+            cv::Mat temp;
+            temp = resized_image_floats - mean;
+            normalized_image = temp / theta;
+            return normalized_image;
+        }
+
+        yoloresults* forward_full(cv::Mat frame, float thresh)
+        {
+            // get height/width dynamically
+            cv::Size image_size = frame.size();
+            float img_height = static_cast<float>(image_size.height);
+            float img_width = static_cast<float>(image_size.width);
+
+            //Set constants and variables -- this is a prob,
+            // how to do this at runtime?
+            //constexpr int const_dtype_code = dtype_code;
+            //constexpr int const_dtype_bits = dtype_bits;
+            //constexpr int const_dtype_lanes = dtype_lanes;
+            //constexpr int const_device_type = device_type;
+            //constexpr int const_device_id = device_id;
+
+            DLTensor *output_tensor_ids;
+            DLTensor *output_tensor_scores;
+            DLTensor *output_tensor_bboxes;
+            DLTensor *input;
+            float *data_x = (float *) malloc(total_input * sizeof(float));
+           
+            // allocate memory for results
+            yoloresults* results = (yoloresults*)calloc(1, sizeof(yoloresults));
+            results->num = 100;
+            results->boxes = (bbox_result*)calloc(100, sizeof(bbox_result));
+
+            // allocate DLTensor memory on device for all the vars needed
+            TVMArrayAlloc(in_shape, in_ndim, dtype_code, dtype_bits, dtype_lanes, device_type, device_id, &input);
+            TVMArrayAlloc(tvm_id_and_score_size, out_ndim, dtype_code, dtype_bits, dtype_lanes, device_type, device_id, &output_tensor_ids);
+            TVMArrayAlloc(tvm_id_and_score_size, out_ndim, dtype_code, dtype_bits, dtype_lanes, device_type, device_id, &output_tensor_scores);
+            TVMArrayAlloc(tvm_box_size, out_ndim, dtype_code, dtype_bits, dtype_lanes, device_type, device_id, &output_tensor_bboxes);
+
+            //copy processed image to DLTensor
+            cv::Mat processed_image = preprocess_image(frame);
+            cv::Mat split_mat[3];
+            cv::split(processed_image, split_mat);
+            memcpy(data_x, split_mat[2].ptr<float>(), processed_image.cols * processed_image.rows * sizeof(float));
+            memcpy(data_x + processed_image.cols * processed_image.rows, split_mat[1].ptr<float>(),
+                   processed_image.cols * processed_image.rows * sizeof(float));
+            memcpy(data_x + processed_image.cols * processed_image.rows * 2, split_mat[0].ptr<float>(),
+                   processed_image.cols * processed_image.rows * sizeof(float));
+            TVMArrayCopyFromBytes(input, data_x, total_input * sizeof(float));
+
+            // standard tvm module run
+            // get the module, set the module-input, and run the function
+            // this is symbolic it ISNT run until TVMSync is performed
+            tvm::runtime::Module *mod = (tvm::runtime::Module *) handle.get();
+            tvm::runtime::PackedFunc set_input = mod->GetFunction("set_input");
+            set_input("data", input);
+            tvm::runtime::PackedFunc run = mod->GetFunction("run");
+            run();
+            tvm::runtime::PackedFunc get_output = mod->GetFunction("get_output");
+
+            // https://github.com/apache/incubator-tvm/issues/979?from=timeline
+            //"This may give you some ideas to start with.
+            //In general you want to use pinned memory and you want
+            //to interleave computation with copying; so you want to
+            // be upload the next thing while you are computing the
+            //current thing while you are downloading the last thing."
+            TVMSynchronize(device_type, device_id, nullptr);
+            get_output(0, output_tensor_ids);
+            get_output(1, output_tensor_scores);
+            get_output(2, output_tensor_bboxes);
+
+            // dynamically set?
+            torch::Tensor ndarray_ids = torch::zeros({1, 100, 1}, at::kFloat);
+            torch::Tensor ndarray_scores = torch::zeros({1, 100, 1}, at::kFloat);
+            torch::Tensor ndarray_bboxes = torch::zeros({1, 100, 4}, at::kFloat);
+
+            TVMArrayCopyToBytes(output_tensor_ids, ndarray_ids.data_ptr(),  1 * 100 * 1 * sizeof(float));
+            TVMArrayCopyToBytes(output_tensor_scores, ndarray_scores.data_ptr(),  1 * 100 * 1 * sizeof(float));
+            TVMArrayCopyToBytes(output_tensor_bboxes, ndarray_bboxes.data_ptr(),  1 * 100 * 4 * sizeof(float));
+
+            auto ndarray_scores_a = ndarray_scores.accessor<float,3>();
+            auto ndarray_ids_a = ndarray_ids.accessor<float,3>();
+            auto ndarray_bboxes_a = ndarray_bboxes.accessor<float,3>();
+
+            int new_num = 0;
+            //int num = 100;
+            for (int i = 0; i < max_yolo_boxes; ++i) {
+                float xmin;
+                float ymin;
+                float xmax;
+                float ymax;
+
+                float score = ndarray_scores_a[0][i][0]; //TODO change 00i
+                float label = ndarray_ids_a[0][i][0];
+                if (score < thresh) continue;
+                if (label < 0) continue;
+
+                int cls_id = static_cast<int>(label);
+                xmin = ndarray_bboxes_a[0][i][0];
+                ymin = ndarray_bboxes_a[0][i][1];
+                xmax = ndarray_bboxes_a[0][i][2];
+                ymax = ndarray_bboxes_a[0][i][3];
+
+                results->boxes[i].xmin = xmin * (img_width/height); // move down to 480 space  ()
+                results->boxes[i].ymin = ymin / (width/img_height); // move up to 640
+                results->boxes[i].xmax = xmax * (img_width/height);
+                results->boxes[i].ymax = ymax / (width/img_height);                
+
+                //results->boxes[i].xmin = xmin * (640.0/512.0); // move down to 480 space  ()
+                //results->boxes[i].ymin = ymin / (512.0/480.0); // move up to 640
+                //results->boxes[i].xmax = xmax * (640.0/512.0);
+                //results->boxes[i].ymax = ymax / (512.0/480.0);
+                results->boxes[i].id = cls_id;
+                results->boxes[i].score = score;
+                new_num+=1;
+            };
+            results->num = new_num;
+           
+            // free outputs
+            TVMArrayFree(input);
+            TVMArrayFree(output_tensor_ids);
+            TVMArrayFree(output_tensor_scores);
+            TVMArrayFree(output_tensor_bboxes);
+            input = nullptr;
+            output_tensor_ids = nullptr;
+            output_tensor_scores = nullptr;
+            output_tensor_bboxes = nullptr;
+            free(data_x);
+            data_x = nullptr;
+            return results;
+        }  
+};    
 
 /**
  * @brief The TVMDetectionNode
@@ -945,7 +1170,8 @@ class TVMDetectionNode {
 private:
   ros::NodeHandle node_;
   // detector
-  std::unique_ptr<YoloTVMGPU256> tvm_object_detector;
+  //std::unique_ptr<YoloTVMGPU256> tvm_object_detector;
+  std::unique_ptr<YoloTVMFromConfig> tvm_object_detector;
   // TF listener
   tf::TransformListener tf_listener;
   // only need this if I need to debug
@@ -1076,7 +1302,8 @@ public:
       cfg_server.setCallback(boost::bind(&TVMDetectionNode::cfg_callback, this, _1, _2));
 
       // create object-detector pointer
-      tvm_object_detector.reset(new YoloTVMGPU256(model_folder_path));
+      tvm_object_detector.reset(new YoloTVMFromConfig("cfg/model.json", "recognition"))
+      //tvm_object_detector.reset(new YoloTVMGPU256(model_folder_path));
       sensor_name = sensor_string;
    }
 
