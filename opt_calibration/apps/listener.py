7aad28d0cfcbb6bc14d88fee2c9e1b606f6afd4b
Aaron Molitor <ammolitor@gmail.com>
2020-05-07 22:14:26 -0500

    update listener to pass serial number (as sensor) in to kinect2_bridge_ir properly

--- opt_calibration/apps/listener.py
+++ opt_calibration/apps/listener.py
@@ -192,7 +192,7 @@ class Listener :
       file.write('  <!-- Launch sensor -->\n')
       file.write('  <include file="$(find kinect2_bridge)/launch/kinect2_bridge_ir.launch">\n')
       if request.serial != '':
-        file.write('    <arg name="sensor_id"           value="$(arg sensor_id)" />\n')
+        file.write('    <arg name="sensor"           value="$(arg sensor_id)" />\n')
       #  file.write('    <arg name="rgb_camera_info_url" value="file://$(find opt_calibration)/camera_info/rgb_$(arg sensor_serial).yaml" />\n')
       #else:
       #  file.write('    <arg name="rgb_camera_info_url" value="file://$(find opt_calibration)/camera_info/rgb_$(arg sensor_id).yaml" />\n')
@@ -325,7 +325,7 @@ class Listener :
       file.write('  <!-- Launch the sensor -->\n')
       file.write('  <include file="$(find kinect2_bridge)/launch/kinect2_bridge_ir.launch">\n')
       if request.serial != '':
-        file.write('  <arg name="sensor_id"           value="$(arg sensor_id)" />\n')
+        file.write('  <arg name="sensor"           value="$(arg sensor_id)" />\n')
       file.write('    <arg name="sensor_name"         value="$(arg sensor_name)" />\n')
       file.write('    <arg name="publish_frame"       value="true" />\n')
       file.write('  </include>\n\n')
