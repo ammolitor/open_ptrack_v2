691e869f170215db9a5d5bd8b2484aeda317f517
Matt <mkrzus3@gmail.com>
2020-06-10 17:39:10 -0500

    adding yolo based calibration - 6

--- tracking/apps/tracker_object_node.cpp
+++ tracking/apps/tracker_object_node.cpp
@@ -236,7 +236,7 @@ detection_cb(const opt_msgs::DetectionArray::ConstPtr& msg)
   if (pos != std::string::npos)
   frame_id_tmp.replace(pos, std::string("_depth_optical_frame").size(), "");
   last_received_detection_[frame_id_tmp] = frame_time;
-
+  frame_id = frame_id_tmp;
   // Compute delay of detection message, if any:
   double time_delay = 0.0;
   if (frame_time > latest_time)
