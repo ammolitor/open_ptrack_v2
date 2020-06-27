d2d3eeade2efb0e3c7cad0de78dbd52820eaf83d
Matt <mkrzus3@gmail.com>
2020-06-26 20:10:45 -0500

    adding debugers to tracking -16

--- recognition/apps/debug_pose_model.cpp
+++ recognition/apps/debug_pose_model.cpp
@@ -945,7 +945,7 @@ class NoNMSPoseFromConfig{
             pose_height = model_config["pose_height"]; //(256, 192)
             gpu = model_config["gpu"];
             n_dets = model_config["n_dets"];
-            no_nms_output_size[3] = {1, n_dets, 6};
+            no_nms_output_size[1] = n_dets;
             detector_total_input = 1 * 3 * detector_width * detector_height;
             pose_total_input = 1 * 3 * pose_width * pose_height;
 
