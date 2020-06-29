3286676de9de58fcd3d15f2368094239e0bc2ed9
Matt <mkrzus3@gmail.com>
2020-06-29 10:44:11 -0500

    Adding base for refactor

--- tvm_models/compile.py
+++ tvm_models/compile.py
@@ -372,6 +372,11 @@ def compile_nonms_object_detector(use_compiler=False):
     print(target)
     block = yolo3_mobilenet1_0_coco(pretrained=True)
     block.hybridize(static_alloc=True)
+    x = np.random.randn(MODEL_CONFIG["object_detector"]["shape"])
+    x = mx.nd.array(x)
+    x = block(x)
+    N_DETS = x.shape[1]
+    print(N_DETS)
     mod, params = relay.frontend.from_mxnet(block, shape={'data': MODEL_CONFIG["nonms_object_detector"]["shape"]}, dtype='float32')
     #net = mod["main"]
     # fused_nn_softmax: num_args should be 4
