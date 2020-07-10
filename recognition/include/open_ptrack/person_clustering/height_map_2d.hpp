342e8e7a1327bfd47b528495dfafd3b57a451600
Matt <mkrzus3@gmail.com>
2020-07-10 09:35:15 -0500

    detection_node: clean -25: starting headbased debug

--- recognition/include/open_ptrack/person_clustering/height_map_2d.hpp
+++ recognition/include/open_ptrack/person_clustering/height_map_2d.hpp
@@ -95,7 +95,6 @@ open_ptrack::person_clustering::HeightMap2D<PointT>::compute (open_ptrack::perso
 
   // make sure cluster works
   std::cout << "[open_ptrack::person_clustering::HeightMap2D::compute] buckets resized!" << std::endl;
-  cluster.getIndices().indices.begin()
   std::cout << "[open_ptrack::person_clustering::HeightMap2D::compute]  cluster.getIndices().indices.begin()!" <<  cluster.getIndices().indices.begin() << std::endl;
   for(std::vector<int>::const_iterator pit = cluster.getIndices().indices.begin(); pit != cluster.getIndices().indices.end(); pit++)
   {
