e12e8ff40dd4d6d8067a256d2d42a7c5933a95a7
Matt <mkrzus3@gmail.com>
2020-07-10 09:32:41 -0500

    detection_node: clean -24: starting headbased debug

--- recognition/include/open_ptrack/person_clustering/height_map_2d.hpp
+++ recognition/include/open_ptrack/person_clustering/height_map_2d.hpp
@@ -95,8 +95,11 @@ open_ptrack::person_clustering::HeightMap2D<PointT>::compute (open_ptrack::perso
 
   // make sure cluster works
   std::cout << "[open_ptrack::person_clustering::HeightMap2D::compute] buckets resized!" << std::endl;
+  cluster.getIndices().indices.begin()
+  std::cout << "[open_ptrack::person_clustering::HeightMap2D::compute]  cluster.getIndices().indices.begin()!" <<  cluster.getIndices().indices.begin() << std::endl;
   for(std::vector<int>::const_iterator pit = cluster.getIndices().indices.begin(); pit != cluster.getIndices().indices.end(); pit++)
   {
+    // it's failing right here. 
     PointT* p = &cloud_->points[*pit];
     // make sure point works
     std::cout << "[open_ptrack::person_clustering::HeightMap2D::compute] x: " << p->x << " y: " << p->y << " z: " << p->z << std::endl;
