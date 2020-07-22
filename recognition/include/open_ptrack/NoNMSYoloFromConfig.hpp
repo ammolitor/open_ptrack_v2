a70bfae674dde5030d6471578126e32ec998a2e6
Matt <mkrzus3@gmail.com>
2020-07-22 12:17:53 -0500

    detection_node: -176: adding manual ground point selection

--- recognition/include/open_ptrack/NoNMSYoloFromConfig.hpp
+++ recognition/include/open_ptrack/NoNMSYoloFromConfig.hpp
@@ -280,7 +280,8 @@ namespace open_ptrack
                 std::vector<open_ptrack::nms_utils::sortable_result> tvm_results;
                 std::vector<open_ptrack::nms_utils::sortable_result> proposals;
                 proposals.clear();
-                open_ptrack::nms_utils::tvm_nms_cpu(proposals, yolo_output, override_threshold, nms_threshold, tvm_results);
+                //open_ptrack::nms_utils::tvm_nms_cpu(proposals, yolo_output, override_threshold, nms_threshold, tvm_results);
+                open_ptrack::nms_utils::opencv_nms(yolo_output, override_threshold, nms_threshold, tvm_results);
                 //std::cout << "ending nms" << std::endl;
 
                 TVMArrayFree(input);
